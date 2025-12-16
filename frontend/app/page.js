e73c1ce270e3109b0b2eeaf10c0f55182f6942e5
damnbro <mohamadshihabi2005@gmail.com>
2025-12-16 16:43:42 +0100

    Added leaflet map to the frontend
    Added leaflet map to the frontend and have added location getter

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -1,9 +1,14 @@
+"use client"
+
+import dynamic from "next/dynamic"
+
+// Loads in Leaflet in Next.js
+const MapView = dynamic(() => import("@/components/map-view"), { ssr: false })
+
 export default function Home() {
   return (
-    <div className="flex h-dvh justify-center items-center">
-      <div className="bg-blue-500/20 border border-blue-500 p-4">
-        <h1 className="text-lg">Eventmap</h1>
-      </div>
+    <div className="h-screen w-full bg-black">
+      <MapView />
     </div>
-  );
+  )
 }
