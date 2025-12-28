642be66dbacd41985d0b9971e8c71587ccbcb2be
ThorANilsson <thor.a.nilsson@gmail.com>
2025-12-28 13:15:11 +0100

    Converted frontend from Javascript to Typescript
    Primary reason is to make it easier to work with the complex objects that will be returned by the API

---
+++ frontend/app/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import dynamic from "next/dynamic";
+
+const MapView = dynamic(() => import("@/components/MapView"), { ssr: false });
+
+export default function Home() {
+  return (
+    <div className="h-screen w-full bg-black">
+      <MapView />
+    </div>
+  );
+}
---
+++ frontend/components/MapView.tsx
@@ -0,0 +1,50 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { MapContainer, TileLayer, Marker, Popup, useMap } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import L from "leaflet";
+
+const icon = L.icon({
+  iconUrl: "https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon.png",
+  iconRetinaUrl:
+    "https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon-2x.png",
+  shadowUrl: "https://unpkg.com/leaflet@1.7.1/dist/images/marker-shadow.png",
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+});
+
+function LocationPin() {
+  const [position, setPosition] = useState(null);
+  const map = useMap();
+
+  useEffect(() => {
+    map.locate().on("locationfound", function (e) {
+      setPosition(e.latlng);
+      map.flyTo(e.latlng, 12);
+    });
+  }, [map]);
+
+  return position === null ? null : (
+    <Marker position={position} icon={icon}>
+      <Popup>I have found u :)</Popup>
+    </Marker>
+  );
+}
+
+export default function MapView() {
+  return (
+    <MapContainer
+      center={[51.505, -0.09]}
+      zoom={5}
+      style={{ height: "100%", width: "100%" }}
+      attributionControl={false}
+    >
+      <TileLayer
+        url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      />
+      <LocationPin />
+    </MapContainer>
+  );
+}
