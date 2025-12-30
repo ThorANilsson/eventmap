90d1fda63d5c6fb12eddfd465addf07a1183d3a6
ThorANilsson <thor.a.nilsson@gmail.com>
2025-12-30 09:35:22 +0100

    Frontend now keeps track of current location and zoom and stores in state. Also makes request to backend based on this info each time you move the map.

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,13 +1,73 @@
 "use client";
 
+import { Button } from "@/components/ui/button";
 import dynamic from "next/dynamic";
+import { useEffect, useState } from "react";
 
 const MapView = dynamic(() => import("@/components/MapView"), { ssr: false });
 
 export default function Home() {
+  const [map, setMap] = useState<L.Map | null>(null);
+  const [center, setCenter] = useState<L.LatLng | null>(null);
+  const [zoom, setZoom] = useState<number | null>(null);
+  const [events, setEvents] = useState<Event[]>([]);
+
+  const handleChange = (newCenter: L.LatLng, newZoom: number) => {
+    setCenter(newCenter);
+    setZoom(newZoom);
+  };
+
+  function getCurrentRadius() {
+    var mapBoundNorthEast = map.getBounds().getNorthEast();
+    var mapDistance = mapBoundNorthEast.distanceTo(map.getCenter());
+    return Math.ceil(mapDistance / 1000);
+  }
+
+  const fetchEvents = async () => {
+    try {
+      /* setLoading(true);
+      setError(null); */
+      const res = await fetch(
+        `http://localhost:5120/Events?Radius=${getCurrentRadius()}&Latitude=${
+          center.lat
+        }&Longitude=${center.lng}`
+      );
+      if (!res.ok) {
+        throw new Error(`API error: ${res.status}`);
+      }
+      setEvents([]);
+      const data = await res.json();
+      setEvents(data);
+    } catch (err: any) {
+      console.log(err);
+      /* setError(err.message); */
+    } finally {
+      /* setLoading(false); */
+    }
+  };
+
+  useEffect(() => {
+    if (!map) return;
+
+    fetchEvents();
+  }, [map, center, zoom]);
+
   return (
-    <div className="h-screen w-full bg-black">
-      <MapView />
+    <div style={{ display: "flex", height: "100vh", width: "100vw" }}>
+      <div style={{ width: "350px", padding: "1rem" }}>
+        {center && zoom !== null && (
+          <>
+            <p>lat: {center.lat}</p>
+            <p>lng: {center.lng}</p>
+            <p>zoom: {zoom}</p>
+            <p>current radius: {getCurrentRadius()} km</p>
+          </>
+        )}
+      </div>
+
+      <div style={{ flex: 1 }}>
+        <MapView onMapReady={setMap} onChange={handleChange} />
+      </div>
     </div>
   );
 }
--- frontend/components/MapView.tsx
+++ frontend/components/MapView.tsx
@@ -1,50 +1,69 @@
 "use client";
 
 import { useEffect, useState } from "react";
-import { MapContainer, TileLayer, Marker, Popup, useMap } from "react-leaflet";
+import {
+  MapContainer,
+  TileLayer,
+  Marker,
+  Popup,
+  useMap,
+  useMapEvents,
+} from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import L from "leaflet";
 
-const icon = L.icon({
-  iconUrl: "https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon.png",
-  iconRetinaUrl:
-    "https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon-2x.png",
-  shadowUrl: "https://unpkg.com/leaflet@1.7.1/dist/images/marker-shadow.png",
-  iconSize: [25, 41],
-  iconAnchor: [12, 41],
-});
+interface MapViewProps {
+  onMapReady: (map: L.Map) => void;
+  onChange: (center: L.LatLng, zoom: number) => void;
+}
 
-function LocationPin() {
-  const [position, setPosition] = useState(null);
+interface MapEventsProps {
+  onChange: (center: L.LatLng, zoom: number) => void;
+}
+
+function MapEvents({ onChange }: MapEventsProps) {
+  const handle = (map: L.Map) => {
+    const c = map.getCenter();
+    const center = L.latLng(Number(c.lat.toFixed(3)), Number(c.lng.toFixed(3)));
+    const zoom = map.getZoom();
+    onChange(center, zoom);
+  };
+
+  useMapEvents({
+    moveend() {
+      handle(this);
+    },
+    zoomend() {
+      handle(this);
+    },
+  });
+
+  return null;
+}
+
+function MapInitializer({ onMapReady }) {
   const map = useMap();
 
   useEffect(() => {
-    map.locate().on("locationfound", function (e) {
-      setPosition(e.latlng);
-      map.flyTo(e.latlng, 12);
-    });
-  }, [map]);
+    onMapReady(map);
+  }, [map, onMapReady]);
 
-  return position === null ? null : (
-    <Marker position={position} icon={icon}>
-      <Popup>I have found u :)</Popup>
-    </Marker>
-  );
+  return null;
 }
 
-export default function MapView() {
+export default function MapView({ onMapReady, onChange }: MapViewProps) {
   return (
     <MapContainer
-      center={[51.505, -0.09]}
-      zoom={5}
+      center={[55.5963028544093, 13.010988504445788]}
+      zoom={13}
+      minZoom={8}
       style={{ height: "100%", width: "100%" }}
       attributionControl={false}
     >
-      <TileLayer
-        url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-      />
-      <LocationPin />
+      <TileLayer url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png" />
+
+      <MapInitializer onMapReady={onMapReady} />
+      <MapEvents onChange={onChange} />
     </MapContainer>
   );
 }
