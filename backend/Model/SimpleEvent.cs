cede7bd57befada02255aa0499e8b84c755adb70
ThorANilsson <thor.a.nilsson@gmail.com>
2026-01-03 20:40:34 +0100

    Created model SimpleEvent
    Makes it much easier to work with events in the current stage

---
+++ backend/Model/SimpleEvent.cs
@@ -0,0 +1,15 @@
+﻿using EventmapApi.Model.Ticketmaster;
+
+namespace EventmapApi.Model
+{
+    public class SimpleEvent
+    {
+        public required string Id { get; set; }
+
+        public string? Name { get; set; }
+
+        public string? VenueName { get; set; }
+
+        public Location? Location { get; set; }
+    }
+}
