739d3b655fd4e258c102f1e7f1a9cd4abb447488
damnbro <mohamadshihabi2005@gmail.com>
2026-01-06 18:37:51 +0100

    Changed the pin on  the map for events

--- frontend/components/TicketmasterIcon.tsx
+++ frontend/components/TicketmasterIcon.tsx
@@ -2,8 +2,8 @@ import L from "leaflet";
 
 const TicketmasterIcon = L.icon({
   iconUrl:
-    "https://play-lh.googleusercontent.com/KmWVboPY-BCCfiflJ-AYCPGBv86QLMsXsSpvQksC0DVR8ENV0lh-lwHnXrekpHwbQA=s256-rw",
-  iconSize: [32, 32],
+    "/mappin.png",
+  iconSize: [22, 22],
   iconAnchor: [16, 32],
   popupAnchor: [0, -32],
 });
